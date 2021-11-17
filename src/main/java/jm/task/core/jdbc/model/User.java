febc6ad983790b19662a568a4ec734311754ae57
Spatkins <theaisven@gmail.com>
2021-11-17 19:33:49 +1000

    1.0

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -31,7 +31,7 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                ", name='" + name + '\'' +
+                "name='" + name + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", age=" + age +
                 '}';
