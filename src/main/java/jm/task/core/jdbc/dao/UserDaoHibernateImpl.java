6b0cc8645d0a3e1c49ea2c1e50e747d20a9b6d1f
Spatkins <theaisven@gmail.com>
2021-11-19 20:16:12 +1000

    1.1 Hiber

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -22,34 +22,72 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Util.getSession().beginTransaction();
-        Util.getSession().save(new User(name, lastName, age));
-        Util.getSession().getTransaction().commit();
-        System.out.println("User successfully add!" + name);
+        try {
+            Util.getSession().beginTransaction();
+            Util.getSession().save(new User(name, lastName, age));
+            Util.getSession().getTransaction().commit();
+            System.out.println("User successfully add!" + name);
+        } catch (Exception e) {
+            if (Util.getSession() != null) {
+            Util.getSession().getTransaction().rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            Util.getSession().close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try {
         Util.getSession().beginTransaction();
         Util.getSession().delete(Util.getSession().get(User.class, id));
         Util.getSession().getTransaction().commit();
         System.out.println("User successfully deleted!" + id);
+    } catch (Exception e) {
+        if (Util.getSession() != null) {
+            Util.getSession().getTransaction().rollback();
+        }
+        e.printStackTrace();
+    } finally {
+        Util.getSession().close();
+    }
     }
 
     @Override
     public List<User> getAllUsers() {
+        List<User> usersList = null;
+        try {
         Util.getSession().beginTransaction();
-        List<User> usersList = Util.getSession().createQuery("from User").getResultList();
+        usersList = Util.getSession().createQuery("from User").getResultList();
         Util.getSession().getTransaction().commit();
         System.out.println(usersList);
+        return usersList;
+    } catch (Exception e) {
+        if (Util.getSession() != null) {
+        Util.getSession().getTransaction().rollback();
+        }
+        e.printStackTrace();
+        } finally {
+        Util.getSession().close();
+        }
         return usersList;
     }
 
     @Override
     public void cleanUsersTable() {
+        try {
         Util.getSession().beginTransaction();
         Util.getSession().createQuery("delete User").executeUpdate();
         Util.getSession().getTransaction().commit();
         System.out.println("Table successfully cleaned!");
+        } catch (Exception e) {
+        if (Util.getSession() != null) {
+        Util.getSession().getTransaction().rollback();
+        }
+        e.printStackTrace();
+        } finally {
+        Util.getSession().close();
+        }
     }
 }
